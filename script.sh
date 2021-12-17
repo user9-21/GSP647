08705f5376b3442a7edf8e92ad2336223362d16b
user9-21 <92918900+user9-21@users.noreply.github.com>
2021-12-17 14:52:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
+cd $HOME
+rm ./.bash_history
+gcloud config set compute/region us-central1
+gcloud config set compute/zone us-central1-a
+cd GSP002
 gcloud auth list
 gcloud config list project
 gcloud config get-value compute/zone
@@ -8,7 +13,5 @@ echo $PROJECT_ID
 echo $ZONE
 gcloud compute instances create gcelab2 --machine-type n1-standard-2 --zone $ZONE
 sudo apt-get install google-cloud-sdk
-gcloud beta interactive
-gcloud compute ssh gcelab2 --zone $ZONE --quiet
 cd $HOME
-vi ./.bashrc
+vi ./.bash_history
