68a7306effb618e960925ec08ed7e8bdfbdce72f
user9-21 <92918900+user9-21@users.noreply.github.com>
2021-12-17 17:27:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,20 @@
+BLACK=`tput setaf 0`
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
 YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
-BG_RED=`tput setab 1`
 BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
 
 BOLD=`tput bold`
 RESET=`tput sgr0`
@@ -20,15 +31,17 @@ echo $REGION
 echo $ZONE
 
 chmod +x ssh.sh
-echo "${YELLOW}${BOLD}${BG_BLACK} ssh.sh are executable now ${RESET}"
+echo "${YELLOW}${BOLD} ssh.sh are executable now ${RESET}"
 
-echo "${RED}${BOLD}${BG_BLACK}
+echo "${WHITE}${BOLD}${BG_RED}
 Run this in ssh instance
 
 ./ssh.sh
 
  ${RESET}"
 gcloud compute scp ssh.sh centos-clean:~
+gcloud compute instances list
+gcloud compute ssh centos-clean --quiet
 rm $HOME/./.bash_history.swp
 # rm $HOME/./.bash_history
 nano $HOME/./.bash_history
