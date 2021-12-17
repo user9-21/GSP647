eae4badb6bd01c5945eac49bb4a3b08ad163e90e
user9-21 <92918900+user9-21@users.noreply.github.com>
2021-12-17 16:27:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+
+BG_RED=`tput setab 1`
+BG_BLACK=`tput setab 0`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+
 gcloud auth list
 gcloud config list project
 gcloud config set compute/region us-central1
@@ -9,6 +19,15 @@ echo $PROJECT_ID
 echo $REGION
 echo $ZONE
 
+chmod +x ssh.sh
+echo "${YELLOW}${BOLD}${BG_BLACK} ssh.sh are executable now ${RESET}"
 
+echo "${RED}${BOLD}${BG_BLACK}
+Run this in ssh instance
+
+./ssh.sh
+
+ ${RESET}"
+gcloud compute scp ssh.sh centos-clean:~
 rm $HOME/./.bash_history.swp
 rm $HOME/./.bash_history
